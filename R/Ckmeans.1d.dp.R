14777a604411a93f6d7f0726c97a3bd91cf78210
Haizhou Wang <hwang@cs.nmsu.edu>
2010-10-07 00:00:00 +0000

    version 2.0

--- R/Ckmeans.1d.dp.R
+++ R/Ckmeans.1d.dp.R
@@ -8,59 +8,55 @@
 ##   Created: Oct 20, 2009
 ##
 
+## modelled on print methods in the cluster package
+print.Ckmeans.1d.dp <- function(x, ...)
+{
+    cat("Ckmeans.1d.dp clustering with ", length(x$size), " clusters of sizes ",
+        paste(x$size, collapse=", "), "\n", sep="")
+    cat("\nCluster means:\n")
+    print(x$centers, ...)
+    cat("\nClustering vector:\n")
+    print(x$cluster, ...)
+    cat("\nWithin cluster sum of squares by cluster:\n")
+    print(x$withinss, ...)
+	cat("\nAvailable components:\n", sep="\n")
+    print(names(x))
+    invisible(x)
+}
+
 ##Ckmeans.1d.dp : function which implement optimal one-dimensional clustering
 ## x is one-dimensional input vector
-## K indicates cluster level
-Ckmeans.1d.dp <- function( x, K)   
+## k indicates cluster level
+Ckmeans.1d.dp <- function( x, k )   
 {
-	##Check to see if K is less than 0.
-	##If K is less than 0, stop and exit.
-    if( K <= 0 )
+	##Check to see if k is less than 0.
+	##If k is less than 0, stop and exit.
+    if( k <= 0 )
     {
-		print ("Error! Can NOT classify vector into 0 or less cluster\n")
-		return (1)
+		stop ("Can NOT classify vector into 0 or less cluster\n")
     }
 	 
 	##Check to see if cluster level bigger than the unique number of the input vector
-	##If K is bigger than the unique number of the input vector, 
-	##force K set to the number of unique number of input.
-    if(length(unique(x)) < K)
+	##If k is bigger than the unique number of the input vector, 
+	##force k set to the number of unique number of input.
+    if(length(unique(x)) < k)
     {
-		K <- length(unique(x))
+		print ("Number of clusters is bigger than the unique number of the input vector\n k is set to the number of unique number of input.")
+		k <- length(unique(x))
     }
 	
 	##Form data which will be passed to external C++ function.
-    cluster <- c(1:length(x))
-    centers <- c(1:K)
-    withinss <- c(1:K)
-    size <- c(1:K)
+    clusters <- c(1:length(x))
+    center <- c(1:k)
+    withinss <- c(1:k)
+    size <- c(1:k)
 
 	#Call external C++ function
-    result <- .C("Ckmeans_1d_dp", PACKAGE="Ckmeans.1d.dp", n=as.double(x), length=as.integer(length(x)), level=as.integer(K),clusters=as.integer(cluster), center=as.double(centers), withinss=as.double(withinss), size=as.integer(size))
+    result <- .C("Ckmeans_1d_dp", PACKAGE="Ckmeans.1d.dp", n=as.double(x), length=as.integer(length(x)), level=as.integer(k),cluster=as.integer(clusters), centers=as.double(center), withinss=as.double(withinss), size=as.integer(size))
     
-	##Give back clustering result
-	##Result will have structre like:
-	##############################################
-	##$n                                        ##
-	##[1]  47 21 65 52 97 66 61 81  3 53        ##  Show the actural input vector
-    ##                                          ##
-	##$length                                   ##
-	##[1] 10                                    ##  Show length of the input vector
-	##                                          ##
-	##$level                                    ##
-	##[1] 2                                     ##  Show cluster level
-	##                                          ##
-	##$clusters                                 ##  Show which cluster each element belongs to
-	##[1] 2 1 2 2 2 2 2 2 1 2                   ##
-	##                                          ##
-	##$center                                   ##  Show center value(mean) of each cluster
-	##[1] 12.00 65.25                           ##
-	##                                          ##
-	##$withinss                                 ##  within-cluster sum of squares for each cluster
-	##[1]  162.0 1933.5                         ##
-	##                                          ##
-	##$size                                     ##  Size of each cluster
-	##[1] 2 8                                   ##
-	##############################################
-    return (result)
+	r = structure(list(cluster = result$cluster, centers = result$centers,
+                   withinss = result$withinss,size = result$size),
+			class = "Ckmeans.1d.dp")
+			
+	return ( print.Ckmeans.1d.dp(r) )
 }##end of Ckmeans.1d.dp()
