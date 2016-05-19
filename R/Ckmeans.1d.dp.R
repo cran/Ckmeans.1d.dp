98e504e5d334836caab21d83cd0828c077b11ab4
Joe Song <joemsong@cs.nmsu.edu>
2016-05-19 23:22:22 +0000

    version 3.4.0-1

--- R/Ckmeans.1d.dp.R
+++ R/Ckmeans.1d.dp.R
@@ -8,22 +8,31 @@
 ##   Created: Oct 20, 2009
 ##
 
-## modelled on print methods in the cluster package
+## print method for Ckmeans.1d.dp object
 print.Ckmeans.1d.dp <- function(x, ...)
 {
-  if(length(x$size) > 1) {
-    cat("Ckmeans.1d.dp returns ", length(x$size), " clusters, each containing ",
-        paste(x$size, collapse=", "), " elements, respectively.\n", sep="")
-  } else {
-    cat("Ckmeans.1d.dp returns one cluster containing all elements.\n")
-  }
+  with(x, {
+    if(length(size) > 1) {
+      cat("Ckmeans.1d.dp returns", length(size), "optimal clusters of sizes",
+          paste(size, collapse=", "), "\n")
+      cat("  minimum total within-cluster sum of squares:", tot.withinss, "\n")
+      cat("  maximum between-cluster sum of squares:", betweenss, "\n")
+      cat("  total sum of squares of input vector:", totss, "\n")
+      if(totss > 0) {
+        cat("  maximum (between-SS / total-SS):",
+            round(1000 * betweenss / totss)/10, "%\n")
+      }
+    } else {
+      cat("Ckmeans.1d.dp returns one cluster containing all elements.\n")
+    }
+    cat("\nCluster means:\n")
+    print(centers, ...)
+    cat("\nCluster index associated with each element:\n")
+    print(cluster, ...)
+    cat("\nWithin-cluster sum of squares:\n")
+    print(withinss, ...)
+  })
 
-  cat("\nCluster means:\n")
-  print(x$centers, ...)
-  cat("\nCluster id of each element:\n")
-  print(x$cluster, ...)
-  cat("\nWithin-cluster sum of squares:\n")
-  print(x$withinss, ...)
   cat("\nAvailable components:\n")
   print(names(x))
   invisible(x)
@@ -46,20 +55,20 @@ Ckmeans.1d.dp <- function( x, k=c(1,9) )
     k.max <- k
   }
 
-	##Check to see if k is less than 0.
-	##If k is less than 0, stop and exit.
+  ##Check to see if k is less than 0.
+  ##If k is less than 0, stop and exit.
   if( k.max <= 0 ) {
-		stop ("Can NOT classify vector into 0 or less cluster\n")
+    stop ("Can NOT classify vector into 0 or less cluster\n")
   }
 
   n.unique <- length(unique(x))
-	##Check to see if cluster level bigger than the unique number of the input vector
-	##If k is bigger than the unique number of the input vector,
-	##force k set to the number of unique number of input.
+  ##Check to see if cluster level bigger than the unique number of the input vector
+  ##If k is bigger than the unique number of the input vector,
+  ##force k set to the number of unique number of input.
   if(n.unique < k.min) {
-		warning("Min number of clusters is greater than the unique number of elements in\n",
-        "the input vector, both k.min and k.max are set to the number of\n",
-        "unique number of input values.\n")
+    warning("Min number of clusters is greater than the unique number of elements in\n",
+            "the input vector, both k.min and k.max are set to the number of\n",
+            "unique number of input values.\n")
     k.min <- n.unique
     k.max <- n.unique
   } else if (n.unique >= k.min && n.unique < k.max) {
@@ -69,13 +78,13 @@ Ckmeans.1d.dp <- function( x, k=c(1,9) )
     k.max <- n.unique
   }
 
-	##Form data which will be passed to external C++ function.
+  ##Form data which will be passed to external C++ function.
   clusters <- vector("integer", length(x))
   center <- vector("double", k.max)
   withinss <- vector("double", k.max)
   size <- vector("integer", k.max)
 
-	#Call external C++ function
+  #Call external C++ function
   result <- .C("Ckmeans_1d_dp", PACKAGE="Ckmeans.1d.dp",
                data=as.double(x), length=as.integer(length(x)),
                Kmin=as.integer(k.min), Kmax=as.integer(k.max),
@@ -92,9 +101,13 @@ Ckmeans.1d.dp <- function( x, k=c(1,9) )
     }
   }
 
+  totss <- sum(scale(x, scale=FALSE)^2)
+  tot.withinss <- sum(result$withinss[1:k.opt])
+  betweenss <- totss - tot.withinss
   r <- structure(list(cluster = result$cluster, centers = result$centers[1:k.opt],
-                      withinss = result$withinss[1:k.opt], size = result$size[1:k.opt]),
+                      withinss = result$withinss[1:k.opt], size = result$size[1:k.opt],
+                      totss = totss, tot.withinss = tot.withinss, betweenss = betweenss),
                  class = "Ckmeans.1d.dp")
 
-	return( r )
+  return( r )
 } ##end of Ckmeans.1d.dp()
--- tests/testthat/test_Ckmeans.1d.dp.R
+++ tests/testthat/test_Ckmeans.1d.dp.R
@@ -2,13 +2,67 @@
 #
 # Joe Song
 # Created: May 3, 2016
+# Updated: May 19, 2016
 
 library(testthat)
 library(Ckmeans.1d.dp)
 context("Checking on several examples")
 
+test_that("Given the number of clusters", {
 
-test_that("Testing Ckmeans.1d.dp()", {
+  x <- c(-1, 2, -1, 2, 4, 5, 6, -1, 2, -1)
+
+  result <- Ckmeans.1d.dp(x, 3)
+  expect_equal(result$size, c(4,3,3))
+
+  cluster.truth <- c(1,2,1,2,3,3,3,1,2,1)
+  expect_equal(result$cluster, cluster.truth)
+
+  centers.truth <- c(c(-1, 2, 5))
+  expect_equal(result$centers, centers.truth)
+  withinss.truth <- c(0,0,2)
+  expect_equal(result$withinss, withinss.truth)
+
+  totss.truth <- sum(scale(x, scale=FALSE)^2)
+  expect_equal(result$totss, totss.truth)
+  expect_equal(result$tot.withinss, 2)
+  expect_equal(result$betweenss, totss.truth - sum(withinss.truth))
+
+})
+
+test_that("n<=k", {
+
+  x <- c(3, 2, -5.4, 0.1);
+  res <- Ckmeans.1d.dp(x, 4)
+
+  cluster.truth <- c(4, 3, 1, 2)
+  expect_equal(res$cluster, cluster.truth)
+  centers.truth <- c(-5.4, 0.1, 2, 3)
+  expect_equal(res$centers, centers.truth)
+  withinss.truth <- c(0, 0, 0, 0)
+  expect_equal(res$withinss, withinss.truth)
+  size.truth <- c(1, 1, 1, 1)
+  expect_equal(res$size, size.truth)
+
+})
+
+test_that("k==2", {
+
+  x <- 1:10
+  res <- Ckmeans.1d.dp(x, 2)
+
+  cluster.truth <- c(1, 1, 1, 1, 1, 2, 2, 2, 2, 2)
+  expect_equal(res$cluster, cluster.truth)
+  centers.truth <- c(3, 8)
+  expect_equal(res$centers, centers.truth)
+  withinss.truth <- c(10, 10)
+  expect_equal(res$withinss, withinss.truth)
+  size.truth <- c(5, 5)
+  expect_equal(res$size, size.truth)
+
+})
+
+test_that("k==1", {
 
   x <- c(-2.5, -2.5, -2.5, -2.5)
   res <- Ckmeans.1d.dp(x, 1)
@@ -22,6 +76,14 @@ test_that("Testing Ckmeans.1d.dp()", {
   size.truth <- c(4)
   expect_equal(res$size, size.truth)
 
+  x <- rep(1, 100)
+  result <- Ckmeans.1d.dp(x, 1)
+  expect_equal(result$size, 100)
+
+})
+
+test_that("n==10, k==3", {
+
   x <- c(3, 3, 3, 3, 1, 1, 1, 2, 2, 2)
   res <- Ckmeans.1d.dp(x, 3)
 
@@ -34,17 +96,10 @@ test_that("Testing Ckmeans.1d.dp()", {
   size.truth <- c(3, 3, 4)
   expect_equal(res$size, size.truth)
 
-  x <- c(3.5, 3.6, 3.7, 3.1, 1.1, 0.9, 0.8, 2.2, 1.9, 2.1)
-  res <- Ckmeans.1d.dp(x, k=c(2,5))
+})
 
-  cluster.truth <- c(3, 3, 3, 3, 1, 1, 1, 2, 2, 2)
-  expect_equal(res$cluster, cluster.truth)
-  centers.truth <- c(0.933333333333, 2.066666666667, 3.475000000000)
-  expect_equal(res$centers, centers.truth)
-  withinss.truth <- c(0.0466666666667, 0.0466666666667, 0.2075000000000)
-  expect_equal(res$withinss, withinss.truth)
-  size.truth <- c(3, 3, 4)
-  expect_equal(res$size, size.truth)
+
+test_that("n==14, k==8", {
 
   x <- c(-3, 2.2, -6, 7, 9, 11, -6.3, 75, 82.6, 32.3, -9.5, 62.5, 7, 95.2)
   res <- Ckmeans.1d.dp(x, k=8)
@@ -58,6 +113,38 @@ test_that("Testing Ckmeans.1d.dp()", {
                       0.000000000, 0.000000000, 0.000000000, 0.000000000)
   expect_equal(res$withinss, withinss.truth)
   size.truth <- c(3, 2, 4, 1, 1, 1, 1, 1)
+  expect_equal(res$size, size.truth)
+
+})
+
+test_that("Estimating k example set 1", {
+
+  x <- c(.9, 1, 1.1, 1.9, 2, 2.1)
+  result <- Ckmeans.1d.dp(x, c(1,6))
+  expect_equal(result$size, c(3,3))
+
+  x <- rev(x)
+  result <- Ckmeans.1d.dp(x, c(1,6))
+  expect_equal(result$size, c(3,3))
+
+  x <- 1:10
+  result <- Ckmeans.1d.dp(x, k=c(1,10))
+  expect_equal(result$size, 10)
+
+})
+
+test_that("Estimating k example set 2", {
+
+  x <- c(3.5, 3.6, 3.7, 3.1, 1.1, 0.9, 0.8, 2.2, 1.9, 2.1)
+  res <- Ckmeans.1d.dp(x, k=c(2,5))
+
+  cluster.truth <- c(3, 3, 3, 3, 1, 1, 1, 2, 2, 2)
+  expect_equal(res$cluster, cluster.truth)
+  centers.truth <- c(0.933333333333, 2.066666666667, 3.475000000000)
+  expect_equal(res$centers, centers.truth)
+  withinss.truth <- c(0.0466666666667, 0.0466666666667, 0.2075000000000)
+  expect_equal(res$withinss, withinss.truth)
+  size.truth <- c(3, 3, 4)
   expect_equal(res$size, size.truth)
 
   x <- cos((-10:10))
